2a214ef271fccbd442b7fee14688e1a3e575cb22
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-05-31 15:52:55 -0500

    Update gameinfo.h

--- gameboy_emulator/include/cartdef.h
+++ gameboy_emulator/include/cartdef.h
@@ -2,6 +2,24 @@
 
 #include <definitions.h>
 
+/*
+0100-0103 — Entry point
+0104-0133 — Nintendo logo
+0134-0143 — Title
+013F-0142 — Manufacturer code
+// skip GCB specific code 0143 — CGB flag
+0144-0145 — New licensee code
+0146 — SGB flag
+0147 — Cartridge type
+0148 — ROM size
+0149 — RAM size
+014A — Destination code
+014B — Old licensee code
+014C — Mask ROM version number
+014D — Header checksum
+014E-014F — Global checksum*/
+
+// cant decide on using classes and mapping the hex value or just keep the header in a struct
 typedef struct
 {
     u8 entry[4];
@@ -20,13 +38,62 @@ typedef struct
     u16 global_checksum; 
 } rom_header;
 
+enum class RomType {
+    ROM_ONLY,
+    MBC1,
+    MBC1_RAM,
+    MBC1_RAM_BATTERY,
+    MBC2,
+    MBC2_BATTERY,
+    ROM_RAM1,
+    ROM_RAM_BATTERY1,
+    MMM01,
+    MMM01_RAM,
+    MMM01_RAM_BATTERY,
+    MBC3_TIMER_BATTERY,
+    MBC3_TIMER_RAM_BATTERY2,
+    MBC3,
+    MBC3_RAM2,
+    MBC3_RAM_BATTERY2,
+    MBC5,
+    MBC5_RAM,
+    MBC5_RAM_BATTERY,
+    MBC5_RUMBLE,
+    MBC5_RUMBLE_RAM,
+    MBC5_RUMBLE_RAM_BATTERY,
+    MBC6,
+    MBC7_SENSOR_RUMBLE_RAM_BATTERY,
+    POCKET_CAMERA,
+    BANDAI_TAMA5,
+    HuC3,
+    HuC1_RAM_BATTERY,
+};
+
+/* ROM sizes
+32 KiB	2 (no banking)
+$01	64 KiB	4
+$02	128 KiB	8
+$03	256 KiB	16
+$04	512 KiB	32
+$05	1 MiB	64
+$06	2 MiB	128
+$07	4 MiB	256
+$08	8 MiB	512
+$52	1.1 MiB	72 3
+$53	1.2 MiB	80 3
+$54	1.5 MiB	
+*/
+enum class RomSize {
+    KB64,
+    KB32,
+};
+
 // global_checksum - (big-endian 16 bit) checksum is computed as the sum of all the bytes of the cartridge ROM
 // header_checksum - 8-bit checksum computed from the cartridge header bytes $0134–014C
-// skipping any headers specific to gbc/gba for now
+
 
 #define bool cart_load(char *cart);
 
 #define int u8 cart_read(u16 address);
 #define void cart_write(u16 address, u8 value);
-
 
